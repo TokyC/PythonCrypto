9613942fec005a43e203afaf352c8564ffcaa2d0
Thomasrm23 <57709671+Thomasrm23@users.noreply.github.com>
2021-07-25 17:03:11 +0200

    Update streamlit.py

--- web/streamlit.py
+++ web/streamlit.py
@@ -1,103 +1,48 @@
 import pymongo
 import streamlit as st
 import pandas as pd
+import numpy as np
+import base64
 import matplotlib.pyplot as plt
-
-# Adresse de la base de données
-DATABASE_ADDRESS = "mongodb+srv://Hakim:PythonProject2021@cluster0.hdu6d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-
-
-# Connection à MongoDb
-@st.cache(hash_funcs={pymongo.MongoClient: id})
-def get_client():
-    return pymongo.MongoClient(DATABASE_ADDRESS)
-
-
-# Chargement des données de coinmarket cap avec reformatage des données
-def load_market_data():
-    df_markets = pd.DataFrame(list(db['coinMarket'].find()))
-    df_markets.pop("_id")
-    df_markets.pop("type_7")
-    df_markets.pop("type_24")
-    df_markets.columns = ['Nom', 'Symbole', 'Prix ($)', 'Variation en 24h', 'Variation en 7j', 'Market Cap ($)',
-                          'Volume en 24h ($)']
-    df_markets['Nom'] = df_markets['Nom'].astype(str)
-    df_markets['Symbole'] = df_markets['Symbole'].astype(str)
-
-    for i in range(len(df_markets['Nom'])):
-        df_markets['Nom'][i] = df_markets['Nom'][i][2:len(df_markets['Nom'][i]) - 2]
-
-    for i in range(len(df_markets['Symbole'])):
-        df_markets['Symbole'][i] = df_markets['Symbole'][i][2:len(df_markets['Symbole'][i]) - 2]
-
-    for i in range(len(df_markets['Prix ($)'])):
-        df_markets['Prix ($)'][i] = df_markets['Prix ($)'][i][0][1::].replace(',', '')
-
-    for i in range(len(df_markets['Market Cap ($)'])):
-        df_markets['Market Cap ($)'][i] = df_markets['Market Cap ($)'][i][0][1::].replace(",", "")
-
-    for i in range(len(df_markets['Volume en 24h ($)'])):
-        df_markets['Volume en 24h ($)'][i] = df_markets['Volume en 24h ($)'][i][0][1::].replace(",", "")
-
-    df_markets['Prix ($)'] = df_markets['Prix ($)'].astype(float)
-    df_markets['Variation en 24h'] = df_markets['Variation en 24h'].astype(float)
-    df_markets['Variation en 7j'] = df_markets['Variation en 7j'].astype(float)
-    df_markets['Market Cap ($)'] = df_markets['Market Cap ($)'].astype(float)
-    df_markets['Volume en 24h ($)'] = df_markets['Volume en 24h ($)'].astype(float)
-    return df_markets
-
-
-# On load les datas
-def load_mongo_data():
-    df = pd.DataFrame(list(db["coinMarket"].find()))
-    df.pop("_id")
-    return df
-
-
-# Récupèrer les prix
-def getPrix(symbole):
-    for i in range(len(df_market['Prix ($)'])):
-        if df_market['Symbole'][i] == symbole:
-            return df_market['Prix ($)'][i]
-
-
-def load_google_article():
-    df_gArticles = pd.DataFrame(list(db['articles'].find()))
-    df_gArticles.pop('_id')
-    return df_gArticles
-
-
-def afficherArticle(title, texte, media, full_articles):
-    middle_page.markdown(f"""
-        ### [{title}]({''.join(full_articles)})
-    """)
-    middle_page.markdown(f"""       
-         >-{texte} ***{media}***
-    """)
-
-
+import json
+import time
+from PIL import Image
 
 # Setup de la page
 st.set_page_config(layout="wide")
-st.title("Agrégateur d'informations sur la Cryptommonaie")
-st.markdown("""## Application""")
+st.title("Agrégateur d' information sur la Cryptommonaie")
+st.markdown("""
+## Application
+""")
 
 # La bare d'information
 extend_bar = st.beta_expander("Information")
 extend_bar.markdown("""
-- **Description du projet** : Dans le cadre du cours de Scrpting Python, nous devons développer un projet portant un sujet au choix en python. Nous avons choisi de scrapper des données sur internet et les valoriser.Nous avons opté pour la récupération des données en rapport avec la cryptomonnaie pour permettre une accessibilité et une facilité d'accès à la connaissance de la cryptomonnaie et au suivi des tendances du marché.
-- **Développeurs** : Toky Cedric Andriamahefa, Hakim Saghir et Thomas Rémy
-- **Langage** : Python
-- **Frameworks** : Streamlit, Scrapy 
+- **Description du projet** : Dans le cadre du cours de NoSQL, nous devons scrapper des données sur internet et les valoriser.
+Nous avons choisit de récupérer des données en rapport avec la cryptomonnaie pour permettre une accessibilité et une facilité d'accès à la connaissance de la cryptomonnaie.
+- **Développeurs** : Quentin Pierson et Toky Cedric Andriamahefa
+- **Framework** : Streamlit, Python, Scrapy, 
 - **Base de données** : MongoDB
-- **Sources** : Cryptonaute, CoinMarketCap
+- **Source** : Cryptonaute, GoogleNews, CoinMarketCap
 """)
 
 
 
-# Connexion à la base de données
+# Connection à MongoDb
+@st.cache(hash_funcs={pymongo.MongoClient : id})
+def get_client() :
+    return pymongo.MongoClient(
+ #       "mongodb+srv://thomas_remy:Illico2377@cluster0.agexe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+"mongodb+srv://Toky:PythonProject@cluster0.squca.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+#mongodb+srv://thomas_remy:<password>@cluster0.agexe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
 client = get_client()
-db = client["articles_db"]
+db = client["articles"]
+
+#
+# client = pymongo.MongoClient("mongodb+srv://Toky:<password>@cluster0.squca.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+# db = client.test
+
 
 # Crétation des trois colonnes
 side_part = st.sidebar
@@ -106,39 +51,106 @@ side_part = st.sidebar
 middle_page, right_side = st.beta_columns((2, 1))
 
 # Partie coinMarket
+dbmarket = "coinMarket"
+
+
+# Chargement des données de coinmarket cap avec reformatage des données
+def load_market_data() :
+    df_markets = pd.DataFrame(list(db[dbmarket].find()))
+    df_markets.pop("_id")
+
+    for i in range(len(df_markets['percent_change_24h'])) :
+        df_markets['percent_change_24h'][i] = float(df_markets['percent_change_24h'][i][0])
+        if df_markets['type_24'][i][0] == "icon-Caret-down" :
+            df_markets['percent_change_24h'][i] = df_markets['percent_change_24h'][i] * (-1)
+
+    for i in range(len(df_markets['percent_change_7d'])) :
+        df_markets['percent_change_7d'][i] = float(df_markets['percent_change_7d'][i][0])
+        if df_markets['type_7'][i][0] == 'icon-Caret-down' :
+            df_markets['percent_change_7d'][i] = df_markets['percent_change_7d'][i] * (-1)
+
+    df_markets.pop("type_7")
+    df_markets.pop("type_24")
+
+    df_markets.columns = ['Nom', 'Symbole', 'Prix ($)', 'Variation en 24h', 'Variation en 7j', 'Market Cap ($)',
+                          'Volume en 24h ($)']
+
+    df_markets['Nom'] = df_markets['Nom'].astype(str)
+    df_markets['Symbole'] = df_markets['Symbole'].astype(str)
+
+    for i in range(len(df_markets['Nom'])) :
+        df_markets['Nom'][i] = df_markets['Nom'][i][2 :len(df_markets['Nom'][i]) - 2]
+
+    for i in range(len(df_markets['Symbole'])) :
+        df_markets['Symbole'][i] = df_markets['Symbole'][i][2 :len(df_markets['Symbole'][i]) - 2]
+
+    for i in range(len(df_markets['Prix ($)'])) :
+        df_markets['Prix ($)'][i] = df_markets['Prix ($)'][i][0][1 : :].replace(',', '')
+
+    for i in range(len(df_markets['Market Cap ($)'])) :
+        df_markets['Market Cap ($)'][i] = df_markets['Market Cap ($)'][i][0][1 : :].replace(",", "")
+
+    for i in range(len(df_markets['Volume en 24h ($)'])) :
+        df_markets['Volume en 24h ($)'][i] = df_markets['Volume en 24h ($)'][i][0][1 : :].replace(",", "")
+
+    df_markets['Prix ($)'] = df_markets['Prix ($)'].astype(float)
+    df_markets['Variation en 24h'] = df_markets['Variation en 24h'].astype(float)
+    df_markets['Variation en 7j'] = df_markets['Variation en 7j'].astype(float)
+    df_markets['Market Cap ($)'] = df_markets['Market Cap ($)'].astype(float)
+    df_markets['Volume en 24h ($)'] = df_markets['Volume en 24h ($)'].astype(float)
+
+    return df_markets
+
+
 df_market = load_market_data()
+
 crypto_ordre = sorted(df_market['Symbole'])
 
-
-
 # ** Partie de droite **
-
 # partie 1 du side_part
 side_part.header('Paramètres : ')
-type_of_cryptocurrency = side_part.selectbox('Choisissez une cryptommonaie de réference : ', crypto_ordre)
+type_of_cryptocurrency = side_part.selectbox('Choisissez le type de cryptommonaie : ', crypto_ordre)
 side_part.write("Vous avez choisis : " + type_of_cryptocurrency)
 
+
 # partie 2 du side_part
+# On load les datas depuis la collection séléctionnée
+coll_name = side_part.selectbox("Choisissez la collection: ", db.list_collection_names()[1:3])
+
+def load_mongo_data() :
+    df = pd.DataFrame(list(db[coll_name].find()))
+    df.pop("_id")
+
+    return df
+
+
 article = load_mongo_data()
 
 # Partie 3
-crypto_selected = side_part.multiselect('Choisissez des cryptomonnaies pour comparer : ', crypto_ordre, )
+crypto_selected = side_part.multiselect('Cryptomonnaie ', crypto_ordre,)
 c_selected = crypto_selected
 if not crypto_selected:
     c_selected = ['BTC']
+
 df_crypto_selected = df_market[(df_market['Symbole'].isin(c_selected))]
 interval_pourcentage = side_part.selectbox('La période de la variation', ['7j', '24h'])
-type_variation = {"7j": 'Variation en 7j', "24h": 'Variation en 24h'}
+type_variation = {"7j" : 'Variation en 7j', "24h" : 'Variation en 24h'}
 choix_variation = type_variation[interval_pourcentage]
+
+
 
 # Partie 4
 res_slider = side_part.slider("Choisissez le nombre d'articles à afficher", 1, 50, 1)
 
 
+# Partie centrale
+# 1
+def getPrix(symbole) :
+    for i in range(len(df_market['Prix ($)'])) :
+        if df_market['Symbole'][i] == symbole :
+            return df_market['Prix ($)'][i]
 
-# ** Partie de droite **
 
-# Partie 1
 middle_page.markdown(f"""
 # Prix du {type_of_cryptocurrency}
 """)
@@ -146,7 +158,7 @@ middle_page.markdown(f"""
 # {getPrix(type_of_cryptocurrency)} $
 """)
 
-# Partie 2
+# 2
 middle_page.markdown("""
 ### Le prix des Cryptos séléctionnées
 """)
@@ -154,7 +166,7 @@ middle_page.write('Dimension : ' + str(df_crypto_selected.shape[0]) + 'ligne(s) 
     df_crypto_selected.shape[1]) + 'colonne(s)')
 middle_page.write(df_crypto_selected)
 
-# Partie 3
+# 3
 middle_page.markdown("""
 ### Tableau de la variation du prix
 """)
@@ -165,53 +177,96 @@ middle_page.dataframe(df_variation)
 df_variation['positif_variation_24'] = df_variation['Variation en 24h'] > 0
 df_variation['positif_variation_7'] = df_variation['Variation en 7j'] > 0
 
-# Partie 4
+# 4
 middle_page.markdown("""
 ### Graph des prix
 """)
 df_price = pd.concat([df_crypto_selected.Symbole, df_crypto_selected['Prix ($)']], axis=1)
-
 df_price = df_price.set_index('Symbole')
 
 plt.figure(figsize=(15, 5))
 plt.subplots_adjust(top=1, bottom=0)
-
 df_price['Prix ($)'].plot(kind='bar', color='b')
 middle_page.pyplot(plt)
-
-
 
 # *** Affichage des articles ***
 
 middle_page.title('Articles du jour')
+
+google_collection_name = coll_name
+
+
+def load_google_article() :
+    df_gArticles = pd.DataFrame(list(db[google_collection_name].find()))
+    df_gArticles.pop('_id')
+    return df_gArticles
+
+
+def afficherArticle_db(title, texte,
+                    full_articles) :
+    middle_page.markdown(f"""
+            ### [{title}]({full_articles})
+        """)
+    middle_page.markdown(f"""
+            ### [{texte}]
+        """)
+
+def afficherArticle_google(title,
+                        media,
+                        full_articles
+                    ):
+    middle_page.markdown(f"""
+            ### [{title}]({''.join(full_articles)})
+        """)
+    middle_page.markdown(f"""       
+             >-Source: ***{media}***
+        """)
+
+
 titleCollection = {
-    "collectionReddit": "Reddit",
-    "collectionTwitter": "Twitter"
+    "collectionReddit" : "Reddit",
+    "collectionTwitter" : "Twitter"
 }
 
 df_Garticles = load_google_article()
 
-for i in range(0, res_slider):
+for i in range(res_slider):
     titre = df_Garticles['title'][i]
     text = df_Garticles['texte'][i]
     full_ref = df_Garticles['full_article_ref'][i]
-    afficherArticle(titre[0], text[0], "Cryptonaute", full_ref)
 
+    if google_collection_name == "articles_db":
+        afficherArticle_db(titre[0], text[0],
+                        full_ref
+                        )
+    elif google_collection_name == "googleNews":
+        med = df_Garticles['media'][i]
+        afficherArticle_google(titre[0],
+                    med[0], full_ref
+                    )
 
+#middle_page.dataframe(df_Garticles)
 
 # *** Rightside ***
+right_side.markdown("""
+### Bar plot du % de variation du Prix
+""")
 
-right_side.markdown("""### Bar plot du % de variation du Prix""")
-
-if interval_pourcentage == '7j':
-    right_side.markdown("""*7 derniers jours*""")
+if interval_pourcentage == '7j' :
+    right_side.markdown("""
+    *7 derniers jours*
+    """)
     plt.figure(figsize=(6, 6))
     plt.subplots_adjust(top=1, bottom=0)
-    df_variation['Variation en 7j'].plot(kind='bar', color=df_variation.positif_variation_7.map({True: 'g', False: 'r'}))
+    df_variation['Variation en 7j'].plot(kind='bar',
+                                         color=df_variation.positif_variation_7.map({True : 'g', False : 'r'}))
     right_side.pyplot(plt)
-else:
-    right_side.markdown("""*dernière 24h*""")
+else :
+    right_side.markdown("""
+    *dernière 24h*
+    """)
     plt.figure(figsize=(6, 6))
     plt.subplots_adjust(top=1, bottom=0)
-    df_variation['Variation en 24h'].plot(kind='bar', color=df_variation.positif_variation_24.map({True: 'g', False: 'r'}))
+    df_variation['Variation en 24h'].plot(kind='barh',
+                                          color=df_variation.positif_variation_24.map({True : 'g', False : 'r'}))
     right_side.pyplot(plt)
